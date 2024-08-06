dde5e95f15c2e1e6c2e835a1ed15c5ce9b8d6aad
yaeok <k.yaeo@carep-tech.com>
2024-08-06 21:07:37 +0900

    memoのモデルを実装

---
+++ lib/model/memo.dart
@@ -0,0 +1,13 @@
+class Memo {
+  int id;
+  String content;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  Memo({
+    required this.id,
+    required this.content,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+}
